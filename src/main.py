3d87129db0af48eab850420e9d7dc478e10d7c1a
Jorge Suárez <jsuadia@gmail.com>
2026-02-04 12:16:08 +0100

    feat: expose github status with fastAPI

--- src/main.py
+++ src/main.py
@@ -1,27 +1,23 @@
 from typing import Union
-
 from fastapi import FastAPI
+import requests
 from pydantic import BaseModel
 
 app = FastAPI()
 
+GITHUB_STATUS_URL = "https://www.githubstatus.com/api/v2/status.json"
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+class StatusInfo(BaseModel):
+    indicator: str
+    description: str
+
+class StatusResponse(BaseModel):
+    status: StatusInfo
+    page: dict
+
+@app.get("/github-status", response_model=StatusResponse)
+def read_github_status():
+    response = requests.get(GITHUB_STATUS_URL)
+    return response.json()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
